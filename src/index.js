85cb931689bfa1dc72c66f4677765e11927f63cc
Ben Fowlie <bsfowlie@gmail.com>
2020-06-18 15:53:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue'
 
 import { router } from './_helpers';
 import App from './app/App';
+import {BootstrapVueIcons} from 'bootstrap-vue';
+//import axios from 'axios';
+import VueAxios from './plugins/vue-axios';
+
+
 
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
 // configureFakeBackend();
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(VueAxios);
+
+//Vue.use(axios);
 
 new Vue({
     el: '#app',
