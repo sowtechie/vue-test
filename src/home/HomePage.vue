ee15175164a5ef826e2df814a717e8894b756579
Ben Fowlie <bsfowlie@gmail.com>
2020-06-18 15:41:59 -0400

    Create HomePage.vue

---
+++ src/home/HomePage.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div>
+
+        <p>
+
+            <router-link to="/admin"><span style="color:blue">Go to admin page</span></router-link>
+
+        </p>
+
+        <p>
+
+             <router-link to="/svel"><span style="color:blue">Elastic Rule</span></router-link>
+
+        </p>
+
+    </div>
+
+</template>
+
+
+
+<script>
+
+import { userService } from '../_services';
+
+
+
+export default {
+
+    data () {
+
+        return {
+
+             user: {},
+
+        }
+
+    },
+
+    created () {
+
+        this.user = JSON.parse(localStorage.getItem('user'));
+
+    }
+
+};
+
+</script>
