2550443d2776c954efc8c8c87785106791516c09
Ben Fowlie <bsfowlie@gmail.com>
2020-06-18 15:48:05 -0400

    Create route.js

---
+++ src/pages/route.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import AdminPage from '../pages/AdminPage';
+import HomePage from '../pages/HomePage';
+import HeadersStatus from '../pages/HeadersStatus';
+import SelectedHeaders from '../pages/SelectedHeaders';
+
+
+Vue.use(Router);
+
+export const router = new Router({
+  base: 'vue-admin-ui',
+  mode: 'history',
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/admin', component: AdminPage },
+    { path: '/samples', component:HeadersStatus },
+    //ElasticRule comes here
+    { path: '/selectedHeaders', component:SelectedHeaders },
+    // { path: '/formRule', component:FormRule },
+    // // otherwise redirect to home
+    { path: '*', redirect: '/' }
+  ]
+});
+
+// router.beforeEach((to, from, next) => {
+//   // redirect to login page if not logged in and trying to access a restricted page
+//   const publicPages = ['/login'];
+//   const authRequired = !publicPages.includes(to.path);
+//   const loggedIn = localStorage.getItem('user');
+
+//   if (authRequired && !loggedIn) {
+//     return next({ 
+//       path: '/login', 
+//       query: { returnUrl: to.path } 
+//     });
+//   }
+
+//   next();
+// })
